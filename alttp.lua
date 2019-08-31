e1bd23ad024a952931a5b414269af79b7c1e284f
James Dunne <james.jdunne@gmail.com>
2019-08-31 00:19:34 -0500

    Devise my own unique identifier for screen whether in dark world, light world, overworld, or dungeon.

--- alttp.lua
+++ alttp.lua
@@ -9,21 +9,28 @@
 --mainmemory.read_u8(0x0086); -- X tile position of Link % 0x20
 --mainmemory.read_u8(0x0088); -- Y tile position of Link % 0x20
 
-local xoffs, yoffs;
-local screen, dungeon;
 local y = {};
 local x = {};
 local t = {};
 local state = {};
 
 while true do
-    screen = mainmemory.read_u16_le(0x008A);
-    dungeon = mainmemory.read_u16_le(0x00A0);
+    local in_dark_world = mainmemory.read_u8(0x0FFF);
+    local in_dungeon = mainmemory.read_u8(0x001B);
+    local screen = mainmemory.read_u16_le(0x008A);
+    local dungeon = mainmemory.read_u16_le(0x00A0);
 
-    yoffs = mainmemory.read_s16_le(0x00E8);
-    xoffs = mainmemory.read_s16_le(0x00E2);
+    local screen_jsd = in_dark_world * 0x20000 + in_dungeon * 0x10000;
+    if in_dungeon ~= 0 then
+        screen_jsd = screen_jsd + dungeon;
+    else
+        screen_jsd = screen_jsd + screen;
+    end
 
-    gui.drawText(0, 0, string.format("%04x %04x  x=%04x,y=%04x", screen, dungeon, xoffs, yoffs), null, 0x3F000000, 11);
+    local yoffs = mainmemory.read_s16_le(0x00E8);
+    local xoffs = mainmemory.read_s16_le(0x00E2);
+
+    gui.drawText(0, 0, string.format("%06x  x=%04x,y=%04x", screen_jsd, xoffs, yoffs), null, 0x3F000000, 11);
 
     for i = 1, 16 do
         y[i] = bit.bor(mainmemory.read_u8(0x0D00+i-1), bit.lshift(mainmemory.read_u8(0x0D20+i-1), 8));
